6c54f3c5c0286f48d6317b0e8489cc0b74b60d56
sqlmerr <sqlmerr@ya.ru>
2023-12-17 12:51:18 +0300

    0.1 styles, html

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI(
-    version="0.0.1"
+    version="0.1"
 )
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
