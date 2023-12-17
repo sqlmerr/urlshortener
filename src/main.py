a2447fed68d57b40ce9405afd903db7679e02a8c
sqlmerr <sqlmerr@ya.ru>
2023-12-17 12:29:15 +0300

    0.0.1 Base

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import uvicorn
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(
+    version="0.0.1"
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def root(request: Request):
+    # return {"message": "Hello World!"}
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
